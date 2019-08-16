63f0dbec1b444940be47934371f861f2c3fb813e
32th System <intensonat@gmail.com>
2019-08-16 14:15:17 +0200

    score_uncap: add missing th17 1.00b addresses

--- score_uncap/th17.v1.00b.js
+++ score_uncap/th17.v1.00b.js
@@ -1,10 +1,10 @@
 {
 	"binhacks": {
 		"new_cap": {
-			"addr": ["Rx343e1", "Rx3454f"]
+			"addr": ["Rx343e1", "Rx3454f", "Rx4b261", "Rx33f6d", "Rx33c5e", "Rx33d60", "Rx1b1d3", "Rx32160", "Rx323b2", "Rx1b04d", "Rx212a9", "Rx324e0"]
 		},
 		"new_cap+1": {
-			"addr": ["Rx343e7", "Rx34555", "Rx4b279"]
+			"addr": ["Rx343e7", "Rx34555", "Rx4b279", "Rx33f73", "Rx33c75", "Rx33d50", "Rx1b1d8", "Rx32166", "Rx323b8", "Rx1b07f", "Rx212ae", "Rx324e5", "Rx083a0", "Rx85295", "Rx85e82", "Rx920ec"]
 		}
 	}
 }
