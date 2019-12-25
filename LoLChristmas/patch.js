6d13b3058857222707c5ff3897cfd4def4906296
32th System <intensonat@gmail.com>
2019-12-25 01:47:32 +0100

    Add LoLChristmas without testing

---
+++ LoLChristmas/patch.js
@@ -0,0 +1,10 @@
+{
+	"dependencies": [
+		"nmlgc/base_tsa"
+	],
+	"id": "LoLChristmas",
+	"servers": [
+		"https://raw.githubusercontent.com/32th-System/crap/master/LoLChristmas/"
+	],
+	"title": "Touhou 15 Christmas version"
+}
