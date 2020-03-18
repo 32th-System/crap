a2f51782dcb1a2eda6dbd2943b44699f7ee0965a
32th System <intensonat@gmail.com>
2020-03-18 16:53:52 +0100

    random_sfx

---
+++ random_sfx/patch.js
@@ -0,0 +1,10 @@
+{
+	"dependencies": [
+		"nmlgc/base_tsa"
+	],
+	"id": "random_sfx",
+	"servers": [
+		"https://raw.githubusercontent.com/32th-System/crap/master/random_sfx/"
+	],
+	"title": "(th17)Randomizes the sound played every time a sound is attempted to be played"
+}
---
+++ random_sfx/th17.v1.00b.js
@@ -0,0 +1,11 @@
+{
+	"codecaves": {
+		"sfx_randomizer": "b960764b00b8b0274000ffd083e07f83f84d760383e83289442408585589e583c002ffe0"
+	},
+	"binhacks": {
+		"sfx_randomizer_call": {
+			"code": "e8[codecave:sfx_randomizer] eb f9 90",
+			"addr": [ "Rx6544b", "Rx654eb" ]
+		}
+	}
+}
