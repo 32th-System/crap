2e85372a3519a662b1ef041b9783e3d72847348d
32th System <intensonat@gmail.com>
2020-09-14 15:23:15 +0200

    esc_r: finally, a good patch

--- esc_r/th17.v1.00b.js
+++ esc_r/th17.v1.00b.js
@@ -1,21 +1,15 @@
 {
+	"codecaves": {
+		"esc_r_sound": "51 6a 07 b8 50544600 ffd0 b8 7d284400 ffe0"
+	},
 	"binhacks": {
-		"esc_r_handler": {
-			"addr": "Rx451dc",
-			"code": "c70604000000e921feffff909090909090909090909090909090909090909090909090909090909090909090",
-			"comment": "The game still picks up and ESC + R but will run completely useless code when R is pressed"
+		"handler": {
+			"code": "6a 01 ffb7 e8010000 e8 27130300 6a 01 ffb7 e4010000 e8 1a130300 c705 d4614b00 0a000000 9090909090909090",
+			"addr": "Rx451dc"
 		},
-		"force_confirm_reset_jmp": {
-			"addr": "Rx45395",
-			"code": "e9f7a10000"
-		},
-		"force_confirm_reset": {
-			"addr": "Rx4f591",
-			"code": "3106314604e865c0fcffe9fa5dffff"
-		},
-		"force_select": {
-			"addr": "Rx453e4",
-			"code": "9090"
+		"sound_jump": {
+			"code": "e9[codecave:esc_r_sound]",
+			"addr": "Rx429aa"
 		}
 	}
 }
