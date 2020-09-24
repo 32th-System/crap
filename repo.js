c348e79fd80f0fbec6ea5dbabebe57e8df2643b5
32th System <intensonat@gmail.com>
2020-09-24 21:36:41 +0200

    Bruh moment

--- repo.js
+++ repo.js
@@ -5,11 +5,11 @@
 		"Goast": "lang_en but Goast is back",
 		"LoLChristmas": "Touhou 15 Christmas version",
 		"WBaDDC": "Implements the DDC scoring system in WBaWC",
+		"dmg_debug": "(th17) displays information about the damage you're dealing",
 		"esc_r": "Brings back ESC + R in th17",
 		"random_sfx": "(th17)Randomizes the sound played every time a sound is attempted to be played",
 		"score_uncap": "Moves the score cap to (2^31-2)*10",
-		"th17_motionsickness_go_spin": "Rotates the background according to how you move to make you throw up",
-		"dmg_debug": "(th17) displays information about the damage you're dealing"
+		"th17_motionsickness_go_spin": "Rotates the background according to how you move to make you throw up"
 	},
 	"servers": [
 		"https://raw.githubusercontent.com/32th-System/crap/master/"
