8903b899c6518cc3f4035ef5d88c7c76663791bd
32th System <intensonat@gmail.com>
2021-02-08 21:47:06 +0100

    Update MoFrance

--- esc_r/th17.v1.00b.js
+++ esc_r/th17.v1.00b.js
@@ -1,15 +1,8 @@
 {
-	"codecaves": {
-		"esc_r_sound": "51 6a 07 b8 50544600 ffd0 b8 7d284400 ffe0"
-	},
 	"binhacks": {
 		"handler": {
 			"code": "6a 01 ffb7 e8010000 e8 27130300 6a 01 ffb7 e4010000 e8 1a130300 c705 d4614b00 0a000000 9090909090909090",
 			"addr": "Rx451dc"
-		},
-		"sound_jump": {
-			"code": "e9[codecave:esc_r_sound]",
-			"addr": "Rx429aa"
 		}
 	}
 }
