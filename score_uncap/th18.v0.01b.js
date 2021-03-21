8027ac98910356f6a781d8b6acdb12ba2427d77a
32th System <intensonat@gmail.com>
2021-03-21 18:25:09 +0100

    score_uncap: th18 demo

---
+++ score_uncap/th18.v0.01b.js
@@ -0,0 +1,10 @@
+{
+	"binhacks": {
+		"new_cap": {
+			"addr": [ "Rx1d78a", "Rx1d792", "Rx1d79e", "Rx26e8a", "Rx40dbd", "Rx42fed", "Rx4571a", "Rxaa7ec", "Rxaa868" ]
+		},
+		"new_cap+1": {
+			"addr": [ "Rx1394c", "Rxa5e74" ]
+		}
+	}
+}
