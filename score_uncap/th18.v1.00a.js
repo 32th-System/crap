ef9565deed0a31f4652c354909cff885d14d0cf2
32th System <intensonat@gmail.com>
2021-05-03 20:32:03 +0200

    score_uncap: th18.v1.00a

---
+++ score_uncap/th18.v1.00a.js
@@ -0,0 +1,10 @@
+{
+	"binhacks": {
+		"new_cap": {
+			"addr": [ "Rx2a665", "Rx2a7fd", "Rx30eab", "Rx4476b", "Rx44ad9", "Rx44c00", "Rx462eb", "Rx463b1", "Rx4656e", "Rx46ac6", "Rx46d09", "Rx5f2c4" ]
+		},
+		"new_cap+1": {
+			"addr": [ "Rx19e70", "Rx2a659", "Rx2a80f", "Rx30eb6", "Rx4477a", "Rx44ade", "Rx44c05", "Rx46302", "Rx463a1", "Rx46578", "Rx46ad7", "Rx46d1a", "Rx5f2cf", "Rx97937", "Rx98515", "Rxa4248", "Rxaee78" ]
+		}
+	}
+}
